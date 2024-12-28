f7c258f55fd6e5dbd855013856d1ee489e9fb7c1
Tarik <tarik.jaidani@gmail.com>
2024-12-28 16:09:11 +0100

    #4

---
+++ MyfirstProject/#4.cpp
@@ -0,0 +1,37 @@
+// #4
+
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+
+void ageDetect(int Age, string license)
+{
+
+	if (Age > 21 && license == "yes")
+	{
+		cout << "Hired";
+	}
+	else {
+		cout << "Rejected";
+	}
+};
+int main()
+{
+	int Age;
+	string license;
+
+	cout << "Enter your age: ";
+	cin >> Age;
+	cout << "Do you have a driver License (yes/no): ";
+	cin >> license;
+
+	ageDetect(Age, license);
+
+	return 0;
+
+}
